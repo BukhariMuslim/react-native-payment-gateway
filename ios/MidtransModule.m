4ef4b03491e7e9d59baa1e56e0fad483108bbf43
Bukhari <bukhari@fore.coffee>
2019-03-08 21:55:43 +0700

    Update param

--- ios/MidtransModule.m
+++ ios/MidtransModule.m
@@ -39,7 +39,7 @@ RCT_EXPORT_METHOD(checkOut:(NSDictionary*) optionConect
                   : (RCTResponseSenderBlock)callback){
 
     [CONFIG setClientKey:[optionConect valueForKey:@"clientKey"]
-             environment:[optionConect valueForKey:@"sandbox"] ? MidtransServerEnvironmentSandbox : MidtransServerEnvironmentProduction
+             environment:[[optionConect valueForKey:@"sandbox"] boolValue] ? MidtransServerEnvironmentSandbox : MidtransServerEnvironmentProduction
        merchantServerURL:[optionConect valueForKey:@"urlMerchant"]];
     
     [self setResCallback: callback];
